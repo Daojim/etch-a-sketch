7d8d804aa5226a7aed936bfb61cc7fa4ca79ce1b
Daojim <jimmyddao@gmail.com>
2025-01-16 16:21:09 -0500

    Create loop within loop to make 256 squares

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const container = document.querySelector("#container");
-
-for (i = 0; i < 16; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.textContent = "Test";
-  container.appendChild(square);
+for (let rows = 0; rows < 16; rows++) {
+  for (let columns = 0; columns < 16; columns++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.textContent = "Test";
+    container.appendChild(square);
+  }
 }
