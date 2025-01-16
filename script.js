9f35f9682f55cf2eb63b30d2aa96817d07728c7d
Daojim <jimmyddao@gmail.com>
2025-01-16 16:40:30 -0500

    Change properties of container and grid Make container 800px by 800px with a 5px outline Need to figure out how to make squares fit inside

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ for (let rows = 0; rows < 16; rows++) {
   for (let columns = 0; columns < 16; columns++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.textContent = "Test";
     container.appendChild(square);
   }
 }
