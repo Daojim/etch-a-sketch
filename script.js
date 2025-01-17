12a725c189d07e5282edf9d3a27bab4ce8e737c6
Daojim <jimmyddao@gmail.com>
2025-01-17 18:31:13 -0500

    Add square background to be black on mouseover

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const container = document.querySelector("#container");
 for (let rows = 0; rows < 16; rows++) {
   for (let columns = 0; columns < 16; columns++) {
     const square = document.createElement("div");
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
     square.classList.add("square");
     container.appendChild(square);
   }
